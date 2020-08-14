bf87c7842d465f4458f3809680534ff4ed182a3a
aerosoul <aerosoul94@hotmail.com>
2020-08-14 18:39:42 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tilutil',
+    version='0.5',
+    packages=['til'],
+    url='https://github.com/aerosoul94/tilutil',
+    license='',
+    author='aerosoul',
+    author_email='',
+    description='Python IDA Pro TIL Library',
+    scripts=['scripts/til2h.py']
+)
